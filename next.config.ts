457bffc0b2431158385a14d931f0fc6aa1814ccb
Levi <ym077wi@gmail.com>
2026-02-19 17:38:15 +0300

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "dariy-shop";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
